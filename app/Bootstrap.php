a6a05ad023ab70c7dd53ec81bbc77b42664654e4
Michael Matos <mikematos84@gmail.com>
2016-11-27 14:05:53 -0500

    Removed unnecessary bootstraping code

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -14,52 +14,6 @@ define("VIEWS", APP_ROOT . "/views");
 define("CONTROLLERS", APP_ROOT . "/controllers");
 define("MODELS", APP_ROOT . "/models");
 
-//Check if environment is development and display errors
-function setReporting(){
-    if(DEVELOPMENT_ENVIRONMENT == true){
-        error_reporting(E_ALL);
-        ini_set("display_errors","On");
-    }else{
-        error_reporting(E_ALL);
-        ini_set("display_errors","Off");
-        ini_set("log_errors", "On");
-        ini_set( "error_log", APP_ROOT . DS . "temp" . DS . "logs" . DS . "error.log" );
-    }
-}
-
-/** Check for Magic Quotes and remove them **/
-function stripSlashesDeep($value){
-    $value = is_array($value) ? array_map("stripSlashesDeep", $value) : stripslashes($value);
-    return $value;
-}
-
-/** Remove Magic Qoutes **/
-function removeMagicQoutes(){
-    if(get_magic_quotes_gpc()){
-        $_GET = stripSlashesDeep($_GET);
-        $_POST = stripSlashesDeep($_POST);
-        $_COOKIE = stripSlashesDeep($_COOKIE);
-    }
-}
-
-/** Check register globals and remove them **/
-function unregisterGlobals(){
-    if(ini_get("register_globals")){
-        $array = array("_SESSION","_POST","_GET","_COOKIE","_REQUEST","_SERVER","_ENV","_FILES");
-        foreach($array as $value){
-            foreach($GLOBALS[$value] as $key => $var){
-                if($var === $GLOBALS[$key]){
-                    unset($GLOBALS[$key]);
-                }
-            }
-        }
-    }
-}
-
-setReporting();
-removeMagicQoutes();
-unregisterGlobals();
-
 //load configuration file
 $config = null;
 if(file_exists(APP_ROOT . "/config/app.json")){
