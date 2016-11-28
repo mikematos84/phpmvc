1bdae44dccb24c56c25aeb6d61c5c5bf3a23743f
Michael Matos <mikematos84@gmail.com>
2016-11-27 23:19:36 -0500

    Updated bootstrap

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -1,12 +1,15 @@
 <?php 
-//Environment
+/* Sessions */
+session_start();
+
+/* Environment */
 define("DEVELOPMENT_ENVIRONMENT", true);
 
-//Absolute Urls
+/* Absolute Urls */
 define("BASE_URL", str_replace(strrchr($_SERVER["PHP_SELF"], "/"), "", $_SERVER["PHP_SELF"]));
 define("SITE_URL", "http" . (($_SERVER["SERVER_PORT"]==443) ? "s://" : "://") . $_SERVER["HTTP_HOST"] . str_replace("/public", "", BASE_URL));
 
-//Absolute Pathing
+/* Absolute Patching */
 define("DS", DIRECTORY_SEPARATOR);
 define("ROOT", dirname(dirname(__FILE__)));
 define("APP_ROOT", ROOT . "/app");
@@ -14,15 +17,16 @@ define("VIEWS", APP_ROOT . "/views");
 define("CONTROLLERS", APP_ROOT . "/controllers");
 define("MODELS", APP_ROOT . "/models");
 
-//load configuration file
+/* Load configuration file */
 $config = null;
 if(file_exists(APP_ROOT . "/config/app.json")){
     $config = json_decode(file_get_contents(APP_ROOT . "/config/app.json"));
 }
 
-//load required core files
+/* Load required core files */
 foreach($config->required as $core_file){
     require_once APP_ROOT . DS . "core" . DS . $core_file . ".php";
 }
 
+/* Start app */
 $app = new App();
